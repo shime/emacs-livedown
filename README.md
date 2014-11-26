bda0b0bc9f7188285d39e10002d801af1e520a90
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-11-26 01:08:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# livedown.el
+
+Emacs plugin for [Livedown](https://github.com/shime/livedown).
+
+## Installation
+
+```shell
+git clone git@github.com:shime/emacs-livedown.git ~/.emacs.d/emacs-livedown
+cat <<EOF >> ~/.emacs.d/init.el
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/emacs-livedown"))
+(require 'livedown)
+EOF
+```
+
+## Configuration
+
+This plugin uses some configurable variables, with the following defaults
+
+```elisp
+(custom-set-variables
+ '(livedown:open t)     ; automatically open the browser window
+ '(livedown:port 1337)) ; port for livedown server
+```
+
+You can also call the open/kill functions manually with
+
+```elisp
+M-x livedown:preview
+M-x livedown:kill
+```
+
+## License
+
+MIT
---
+++ livedown.el
@@ -0,0 +1,48 @@
+;;; livedown.el --- Realtime Markdown previews for Emacs.
+
+;; Copyright (C) 2014 Hrvoje Simic
+
+;; Author: Hrvoje Simic <shime.ferovac@gmail.com>
+;; Version: 0.0.0
+;; Keywords: markdown, preview, live
+;; URL: https://github.com/shime/emacs-livedown
+
+;;; Commentary:
+
+;; Realtime Markdown previews for Emacs.
+
+(defgroup livedown nil
+  "Realtime Markdown previews"
+  :group 'livedown
+  :prefix "livedown:")
+
+(defcustom livedown:port 1337
+  "Port on which livedown server will run."
+  :type 'integer
+  :group 'livedown)
+
+(defcustom livedown:open t
+  "Open browser automatically."
+  :type 'boolean
+  :group 'livedown)
+
+(defun livedown:preview ()
+    "Preview the current file in livedown."
+      (interactive)
+        (call-process-shell-command
+             (format "livedown start %s --port %s %s &"
+                            (shell-quote-argument (buffer-file-name))
+                            livedown:port
+                            (if livedown:open "--open" ""))))
+
+(defun livedown:kill ()
+    "Stops the livedown process."
+      (interactive)
+        (call-process-shell-command
+             (format "livedown stop --port %s &"
+                            livedown:port)))
+
+(eval-after-load 'markdown-mode '(livedown:preview))
+(add-hook 'kill-emacs-query-functions (lambda () (livedown:kill)))
+
+(provide 'livedown)
